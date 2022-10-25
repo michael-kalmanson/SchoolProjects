480956840887486079df31694c1d605291276321
michael-kalmanson <104147117+michael-kalmanson@users.noreply.github.com>
2022-10-25 10:31:52 +0300

    Add files via upload

---
+++ DN_IDC_2022C_Ex01/C22 Ex01 OriSheflan 315683326 MichaelKalmanson 208884106/Ex01_03/Program.cs
@@ -0,0 +1,30 @@
+﻿using Ex01_02;
+
+namespace Ex01_03
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int userInputHeightOfDiamond = getUserInput();
+            Ex01_02.Program.Print_diamond(userInputHeightOfDiamond);
+            System.Console.WriteLine("Press enter to terminate program.");
+            System.Console.ReadLine();
+        }
+
+        private static int getUserInput()
+        {
+            System.Console.WriteLine("Please enter requested height of the diamond (and then press enter).");
+            string userInput = System.Console.ReadLine();
+            int intUserInput = 0;
+            while (!int.TryParse(userInput, out intUserInput) || intUserInput < 0)
+            {
+                System.Console.WriteLine("Entered an invalid input, please try again:");
+                System.Console.WriteLine("Please enter requested height of the diamond (and then press enter).");
+                userInput = System.Console.ReadLine();
+            }
+
+            return intUserInput;
+        }
+    }
+}
